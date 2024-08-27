cf3c8eebd5ddbbd9ee47d8f10ebc211b237c85d2
Victor Vieira <victorxdking@gmail.com>
2024-08-27 11:01:57 -0300

    (27/08) ShoppingCart

--- Lista3/src/Main.java
+++ Lista3/src/Main.java
@@ -1,5 +1,6 @@
 import br.edu.fatecfranca.ex0.CarItem;
 import br.edu.fatecfranca.ex0.Product;
+import br.edu.fatecfranca.ex0.ShoppingCart;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,20 +9,16 @@ public class Main {
         Product prod2 = new Product(2, "Teclado", 140);
         Product prod3 = new Product(3, "Mouse", 90);
 
-        CarItem carItem1 = new CarItem(1, 5, prod1);
-        System.out.println(carItem1);
+        ShoppingCart sc1 = new ShoppingCart(1, "Franca", "Cartão de crédito");
+        sc1.addCarItem(1,5,prod1);
+        sc1.addCarItem(3,10,prod2);
+        sc1.calculatePrice();
+        System.out.println(sc1.toString());
 
-        CarItem carItem2 = new CarItem(2, 5, prod1);
-        System.out.println(carItem2);
-
-        CarItem carItem3 = new CarItem(3, 10, prod2);
-        System.out.println(carItem3);
-
-        CarItem carItem4 = new CarItem(4, 15, prod2);
-        System.out.println(carItem4);
-
-        CarItem carItem5 = new CarItem(5, 20, prod3);
-        System.out.println(carItem5);
-
+        ShoppingCart sc2 = new ShoppingCart(2, "Batatais", "PIX");
+        sc2.addCarItem(2,6,prod1);
+        sc2.addCarItem(5,20,prod3);
+        sc2.calculatePrice();
+        System.out.println(sc2.toString());
     }
 }
